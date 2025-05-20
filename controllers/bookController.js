bd2dd393885043b1ff1db201c39eefadbc341322
AriHyuk <coygalang990@gmail.com>
2025-05-20 21:58:38 +0700

    update fungsi crud

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,6 +2,7 @@ const getBooks = (req, res) => {
   res.json([
     { id: 1, title: 'Belajar Express', author: 'Ari Awaludin' },
     { id: 2, title: 'Node.js for Fun', author: 'John Doe' }
+    
   ]);
 };
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("bacot jokowi");
+
 const express = require('express');
 const app = express();
 const bookRoutes = require('./routes/bookRoutes');
